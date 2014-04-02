58886d9ba3e749767f41847c809b8160420ae2cc
ehjung <ejung@thoughtworks.com>
2014-04-02 15:01:35 -0400

    Added tableview for form in storyboard

---
+++ anAverageDay/FormViewController.h
@@ -0,0 +1,15 @@
+//
+//  FormViewController.h
+//  anAverageDay
+//
+//  Created by esther on 4/2/14.
+//  Copyright (c) 2014 ehjung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FormViewController : UITableViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *text;
+
+@end
---
+++ anAverageDay/FormViewController.m
@@ -0,0 +1,13 @@
+//
+//  FormViewController.m
+//  anAverageDay
+//
+//  Created by esther on 4/2/14.
+//  Copyright (c) 2014 ehjung. All rights reserved.
+//
+
+#import "FormViewController.h"
+
+@implementation FormViewController
+
+@end
