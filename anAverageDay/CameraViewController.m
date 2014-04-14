589f292eb14a09554354d6984524356fded0b1ac
ehjung <ejung@thoughtworks.com>
2014-04-14 18:15:11 -0400

    Removed random images used to test stuff

--- anAverageDay/CameraViewController.m
+++ anAverageDay/CameraViewController.m
@@ -91,7 +91,7 @@
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -100,6 +100,6 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+
 
 @end
