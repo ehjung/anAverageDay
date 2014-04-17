708ff67fc51fc6fbb475b1bb80b0d31f0a26831d
ehjung <ejung@thoughtworks.com>
2014-04-17 18:01:15 -0400

    Some code cleanup

--- anAverageDay/CameraViewController.m
+++ anAverageDay/CameraViewController.m
@@ -82,18 +82,5 @@
          [self.imageView setImage:image];
 	 }];
 }
-
-
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-/*
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    NSLog(@"Test");
-    // Pass the selected object to the new view controller.
-}
-*/
-    
+   
 @end
