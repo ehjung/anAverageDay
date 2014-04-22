6b535fb8606c1babc83b2b9d5e6d08f3bfe7a798
ehjung <ejung@thoughtworks.com>
2014-04-21 20:24:13 -0400

    CameraViewController cleanup

--- anAverageDay/CameraViewController.m
+++ anAverageDay/CameraViewController.m
@@ -25,21 +25,16 @@
     return self;
 }
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    UIImage *buttonImage = [UIImage imageNamed:@"button.png"];
+    [self.cameraButton setImage:buttonImage forState:UIControlStateNormal];
+}
+
 - (void)viewDidAppear:(BOOL)animated {
-    // set up video session and video preview layer
-    self.session.sessionPreset = AVCaptureSessionPresetMedium;
-    self.videoLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
-    self.videoLayer.frame = self.imagePreview.bounds;
-    [self.imagePreview.layer addSublayer:self.videoLayer];
-    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-	
-    // add input device
-	NSError *error = nil;
-	AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:&error];
-	if (!input) {
-		NSLog(@"ERROR: trying to open camera: %@", error);
-	}
-	[self.session addInput:input];
+    [self videoSetup];
+	[self addInputDevice];
     
     // add output
     self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
@@ -50,11 +45,22 @@
     [self.session startRunning];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    UIImage *buttonImage = [UIImage imageNamed:@"button.png"];
-    [self.cameraButton setImage:buttonImage forState:UIControlStateNormal];
+- (void)videoSetup {
+    // set up video session and video preview layer
+    self.session.sessionPreset = AVCaptureSessionPresetMedium;
+    self.videoLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+    self.videoLayer.frame = self.imagePreview.bounds;
+    [self.imagePreview.layer addSublayer:self.videoLayer];
+    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+}
+
+- (void)addInputDevice {
+    NSError *error = nil;
+	AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:&error];
+	if (!input) {
+		NSLog(@"ERROR: trying to open camera: %@", error);
+	}
+	[self.session addInput:input];
 }
 
 - (void)didReceiveMemoryWarning
@@ -64,28 +70,26 @@
 }
 
 - (IBAction)captureNow {
-    AVCaptureConnection *videoConnection = nil;
-    for (AVCaptureConnection *connection in self.stillImageOutput.connections)
-	{
-		for (AVCaptureInputPort *port in [connection inputPorts])
-		{
-			if ([[port mediaType] isEqual:AVMediaTypeVideo] )
-			{
-				videoConnection = connection;
+    for (AVCaptureConnection *connection in self.stillImageOutput.connections) {
+		for (AVCaptureInputPort *port in [connection inputPorts]) {
+			if ([[port mediaType] isEqual:AVMediaTypeVideo] ) {
+				self.captureConnection = connection;
 				break;
 			}
 		}
-		if (videoConnection) { break; }
+		if (self.captureConnection) { break; }
 	}
-	
+    [self convertToStillImage];
+}
+
+- (void)convertToStillImage {
     // AVCapture image to imageView image
-	[self.stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler: ^(CMSampleBufferRef imageSampleBuffer, NSError *error)
-     {
-         NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
-         UIImage *image = [[UIImage alloc] initWithData:imageData];
-         image = [[ResizeImage sharedResizeImage] resizeImage:image targetHeight:175 targetWidth:275];
-         [self.imageView setImage:image];
-	 }];
+	[self.stillImageOutput captureStillImageAsynchronouslyFromConnection:self.captureConnection completionHandler: ^(CMSampleBufferRef imageSampleBuffer, NSError *error) {
+        NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
+        UIImage *image = [[UIImage alloc] initWithData:imageData];
+        image = [[ResizeImage sharedResizeImage] resizeImage:image targetHeight:175 targetWidth:275];
+        [self.imageView setImage:image];
+    }];
 }
    
 @end
